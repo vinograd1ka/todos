005345c7cfe4e6f25ac837dafadb81cf5024571a
vinograd1ka <kachanov220@mail.ua>
2020-10-06 13:56:44 +0300

    for cycle example

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
     var addButton = document.getElementById("add-button");
     var nameInput = document.getElementById("name-input");
     var list = document.getElementById("todos-list");
-
-
+    var countingEl = document.getElementById('counting');
+    var count = 0;
+    var deleteButton = document.getElementById('button')
     addButton.addEventListener('click', function () {
 
         var div = document.createElement("div");
@@ -12,15 +13,30 @@
 
             if(div.classList.contains('complete')){
                 //var check = div.getElementsByClassName('check')[0]
-                div.classList.remove("complete");
                 //check.style.display = 'none';
+                div.classList.remove("complete");
+                count--
+                countingEl.innerHTML =
+                    '<div class="counting-header" id="counting">Выбранно: '+count+' </div>'
             }
 
             else{
-                div.classList.add('complete')
                 //check.style.display = 'block';
+                div.classList.add('complete');
+                count++
+                countingEl.innerHTML =
+                    '<div class="counting-header" id="counting">Выбранно: '+count+' </div>'
             }
         };
+        deleteButton.onclick = function() {
+            var addComplete = document.querySelectorAll('.complete');
+            for (let i = 0; i < addComplete.length; i++) {
+                count--;
+                addComplete[i].style.display = 'none';
+
+            }
+        };
+
 
         div.className = "search-container"
         div.innerHTML =
